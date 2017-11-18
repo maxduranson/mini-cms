a9d60d623fecfa84e259bbf2c297779a114f2a10
Maximilien <maxduranson@hotmail.com>
2017-11-18 15:47:52 +0100

    Component LoginForm added in App.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LoginForm from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
