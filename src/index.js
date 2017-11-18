bd57e28ace0f40c5dfabb6b32181f276f489f296
Maximilien <maxduranson@hotmail.com>
2017-11-18 15:55:31 +0100

    updating index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import LoginForm from './App';
+import LoginForm from './App/App.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
